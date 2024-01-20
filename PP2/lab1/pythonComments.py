45fba636232816211d47db74e79eda7dbb5cf3d2
Amirmv <102911636+rmvxmir@users.noreply.github.com>
2024-01-20 17:16:11 +0600

    Delete PP2/lab1 directory

--- PP2/lab1/pythonComments.py
+++
@@ -1,9 +0,0 @@
-#Exercise 1
-#This is a comment
-
-#Exercise 2
-"""
-This is a comment 
-writen in 
-more than just one line
-"""
--- PP2/lab1/pythonSyntax.py
+++
@@ -1,6 +0,0 @@
-#Exercise 1
-print("Hello World")
-
-#Exercise 2
-if 5 > 2:
-    print('YES')
