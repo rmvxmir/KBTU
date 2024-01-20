f13188cb226d30762bd9f7774486c22ced43d792
Amirmv <102911636+rmvxmir@users.noreply.github.com>
2024-01-20 17:15:14 +0600

    Python laboratory work #1

---
+++ PP2/lab1/pythonComments.py
@@ -0,0 +1,9 @@
+#Exercise 1
+#This is a comment
+
+#Exercise 2
+"""
+This is a comment 
+writen in 
+more than just one line
+"""
---
+++ PP2/lab1/pythonDataTypes.py
@@ -0,0 +1,27 @@
+#Exercise 1
+x = 5
+print(type(x)) #outputs: 'int'
+
+#Exercise 2
+x = "Hello World"
+print(type(x)) #outputs: 'str'
+
+#Exercise 3
+x = 20.5
+print(type(x)) #outputs: 'float'
+
+#Exercise 4
+x = ['apple', 'banana', 'cherry']
+print(type(x)) #outputs: 'list'
+
+#Exercise 5
+x = ('apple', 'banana', 'cherry')
+print(type(x)) #outputs: 'tuple'
+
+#Exercise 6
+x = {'name' : 'John', 'age' : '36'}
+print(type(x)) #outputs: 'dict'
+
+#Exercise 7
+x = True
+print(type(x)) #outputs: 'bool'
