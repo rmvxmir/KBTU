9c47c71d839c7f33d6067c311367497eff8049e1
Amirmv <102911636+rmvxmir@users.noreply.github.com>
2024-01-23 12:36:54 +0600

    Add files via upload
    Laboratory work #2

---
+++ lab2/pythonBooleans.py
@@ -0,0 +1,14 @@
+#Exercise 1
+print(10 > 9) #outputs: 'True'
+
+#Exercise 2
+print(10 == 9) #outputs: 'False'
+
+#Exercise 3
+print(10 < 9) #outputs: 'False'
+
+#Exercise 4
+print(bool("abc")) #outputs: 'True'
+
+#Exercise 5
+print(bool(0)) #outputs: 'False'
